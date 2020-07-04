331b1d2ea12d80def90414ec9746078b651f6817
vb <dvavkvadrate@gmail.com>
2020-07-04 04:55:54 +0300

    refactored

--- test/test_income_statement.py
+++ test/test_income_statement.py
@@ -4,11 +4,11 @@ from main import format_number as f
 def test_gross_income():
     ticker = 'epam'
     url = f'https://www.marketwatch.com/investing/stock/{ticker}/financials'
-    content = main.get_content(url)
+    page_content = main.get_content(url)
 
-    sales_revenue = float(f(content.xpath(main.sales_revenue_xpath)[0].text))
-    cost_of_goods = float(f(content.xpath(main.cost_of_goods_xpath)[0].text))
-    gross_income = float(f(content.xpath(main.gross_income_xpath)[0].text))
+    sales_revenue = float(f(page_content.xpath(main.sales_revenue_xpath)[0].text))
+    cost_of_goods = float(f(page_content.xpath(main.cost_of_goods_xpath)[0].text))
+    gross_income = float(f(page_content.xpath(main.gross_income_xpath)[0].text))
 
     expected_gross_income = sales_revenue - cost_of_goods
     actual_gross_income = gross_income
